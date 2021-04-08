1d12b100895f492d7bdfcd0ee1e4016b5e7cdcce
CeeThinwa <45120266+CeeThinwa@users.noreply.github.com>
2021-04-08 18:36:26 +0300

    Add files via upload

---
+++ My Little PDF scraper.py
@@ -0,0 +1,14 @@
+# Install Java first
+# then pip install tabula-py
+# then pip install tabulate
+from tabula import read_pdf
+from tabulate import tabulate
+
+# reads table from pdf file
+path="" #Edit path to where file resides on your computer
+tables = read_pdf(path, pages="all")
+# if the pdf is one table on one page,
+# tables becomes a Pandas dataframe;
+# if the pdf has many pages,
+# tables becomes a Python listy of dataframes
+print(tabulate(tables[0]))
